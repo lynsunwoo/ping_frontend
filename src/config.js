f5a4ea6db784dca5189c7d4cd022db8eddc1871d
rin <rin@rinui-MacBookAir.local>
2026-02-10 12:07:34 +0900

    src 폴더

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://port-0-ping-backend-mlelsdiwef1c9091.sel3.cloudtype.app"
+    ? "https://port-0-ping-backend-mkvwe6z7891e08f1.sel3.cloudtype.app"
     : "http://localhost:9070";
 
 export default BASE_URL;
